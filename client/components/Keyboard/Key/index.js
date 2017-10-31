586ef674c67f05cf1570f95bdc8fab2d0ebf607a
Liam Butterworth <wbbutterworth@gmail.com>
2017-10-31 14:34:49 -0500

    organize and divide logic and event handling between keyboard and keys; start styling the actual keyboard ui and class/state changes

--- client/components/Keyboard/Key/index.js
+++ client/components/Keyboard/Key/index.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React      from 'react';
+import ClassNames from 'classnames';
+import { Note }   from 'music-theory';
 
 require( './style.css' );
 
 class Key extends React.Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			isPressed: false,
+		};
+
+		this.note = new Note( this.props.note );
+	}
+
+	componentWillMount() {
+		document.addEventListener( 'keydown', ( event ) => {
+			if ( event.key.charCodeAt() === this.props.keybind.charCodeAt() && !event.repeat ) this.play();
+		} );
+
+		document.addEventListener( 'keyup', ( event ) => {
+			if ( event.key.charCodeAt() === this.props.keybind.charCodeAt() && !event.repeat ) this.stop();
+		} );
+	}
+
+	play() {
+		this.setState({ isPressed: true })
+		console.log( `play a ${ this.note.symbol() }` );
+	}
+
+	stop() {
+		this.setState({ isPressed: false });
+		console.log( `stop playing a ${ this.note.symbol() }` );
+	}
+
 	render() {
+		const classNames = ClassNames({
+			'keyboard-key':             true,
+			'keyboard-key--accidental': this.note.isAccidental,
+			'keyboard-key--is-pressed': this.state.isPressed,
+		});
+
 		return (
-			<div className="keyboard-key" onKeyPress={ this.keypress }>
-				{ this.props.note.symbol() }
-			</div>
+			<div className={ classNames } />
 		);
 	}
 }
--- client/components/Keyboard/index.js
+++ client/components/Keyboard/index.js
@@ -1,71 +1,52 @@
 import React    from 'react';
 import shortid  from 'shortid';
-import { Note } from 'music-theory';
 import Key      from './Key';
 
 require( './style.css' );
-
-const defaultKeybinds = [
-	{ keybind: '\t', note: 'C' },
-	{ keybind: 'q',  note: 'D' },
-	{ keybind: 'w',  note: 'E' },
-	{ keybind: 'e',  note: 'F' },
-	{ keybind: 'r',  note: 'G' },
-	{ keybind: 't',  note: 'A' },
-	{ keybind: 'y',  note: 'B' },
-	{ keybind: 'u',  note: 'C' },
-	{ keybind: 'i',  note: 'D' },
-	{ keybind: 'o',  note: 'E' },
-	{ keybind: 'p',  note: 'F' },
-	{ keybind: '[',  note: 'G' },
-	{ keybind: ']',  note: 'A' },
-	{ keybind: '\\', note: 'B' },
-	{ keybind: '\b', note: 'C' },
-];
 
 class Keyboard extends React.Component {
 	constructor( props ) {
 		super( props );
 
-		this.handleKeyPress = this.handleKeyPress.bind( this );
-		this.keybinds = this.props.keybinds || defaultKeybinds;
-	}
-
-	componentWillMount() {
-		this.keys = this.keybinds.map( ( keybind ) => {
-			return (
-				<Key
-					key={ shortid.generate() }
-					note={ new Note( keybind.note ) }
-					keybind={ keybind.keybind }
-				/>
-			);
-		} );
-
-		document.addEventListener( 'keypress', this.handleKeyPress );
-	}
-
-	componentWillUnmount() {
-	}
-
-	handleKeyPress( event ) {
-		const keyPressed = this.keys.find( ( key ) => {
-			return key.props.keybind.charCodeAt() === event.charCode;
-		} );
-
-		console.log( `play a ${ keyPressed.props.note.symbol() }` );
-	}
-
-	createKeys() {
-		return Object.keys( this.keys ).map( ( key, index ) => {
-			return ( <Key key={ shortid.generate() } note={ this.keys[ key ] } keybind={ key } /> );
-		} );
+		this.keybinds = this.props.keybinds || [
+			{ keybind: '\t', note: 'C' },
+			{ keybind: '`',  note: 'Db' },
+			{ keybind: 'q',  note: 'D' },
+			{ keybind: '1',  note: 'Eb' },
+			{ keybind: 'w',  note: 'E' },
+			{ keybind: 'e',  note: 'F' },
+			{ keybind: '3',  note: 'Gb' },
+			{ keybind: 'r',  note: 'G' },
+			{ keybind: '4',  note: 'Ab' },
+			{ keybind: 't',  note: 'A' },
+			{ keybind: '5',  note: 'Bb' },
+			{ keybind: 'y',  note: 'B' },
+			{ keybind: 'u',  note: 'C' },
+			{ keybind: '7',  note: 'Db' },
+			{ keybind: 'i',  note: 'D' },
+			{ keybind: '8',  note: 'Eb' },
+			{ keybind: 'o',  note: 'E' },
+			{ keybind: 'p',  note: 'F' },
+			{ keybind: '9',  note: 'Gb' },
+			{ keybind: '[',  note: 'G' },
+			{ keybind: '-',  note: 'Ab' },
+			{ keybind: ']',  note: 'A' },
+			{ keybind: '=',  note: 'Bb' },
+			{ keybind: '\\', note: 'B' },
+			{ keybind: '\b', note: 'C' },
+		];
 	}
 
 	render() {
 		return (
 			<div className="keyboard">
-				{ this.keys }
+				{ this.keybinds.map( ( keybind ) =>
+					<Key
+						key={ shortid.generate() }
+						note={ keybind.note }
+						keybind={ keybind.keybind }
+					/>
+				) }
 			</div>
 		);
 	}
