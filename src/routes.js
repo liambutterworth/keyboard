4da581dffe43e07b6ddf7185b49c14af380b675a
Liam Butterworth <wbbutterworth@gmail.com>
2018-03-30 09:08:00 -0500

    add reference to public_url environment variable in routes.js

--- src/routes.js
+++ src/routes.js
@@ -8,10 +8,12 @@ import {
 	Route,
 } from 'react-router-dom';
 
+console.log( process.env.PUBLIC_URL );
+
 const Routes = () => (
 	<Router>
 		<Switch>
-			<Route path="/" exact component={ Home } />
+			<Route path={ `${ process.env.PUBLIC_URL || '' }/` } exact component={ Home } />
 			<Route component={ NotFound } />
 		</Switch>
 	</Router>
